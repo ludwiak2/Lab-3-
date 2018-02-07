eda06d67c4b3863660f49e24db8c7c903b3d224b
nicoleludwiak <ludwiak2@illinois.edu>
2018-02-07 16:38:26 -0600

    Word Count Specific Word Count

--- src/Myclass.java
+++ src/Myclass.java
@@ -1,5 +1,49 @@
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Myclass {
-    public static void main (String [] array) {
-        
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString ( final String url){
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
+    public static int wordCount(String line) {
+
+
+        String[] words = line.split("\\s+");
+        return words.length;
+
+    }
+
+    public static int specificwordcount (String line , String searchword){
+        int counter = 0;
+        String[] words = line.split("\\s+");
+        for (String word : words )
+            if (word.equals(searchword)){
+            counter++;
+            }
+        return counter;
+    }
+
+    public static void main(String[] array) {
+        String line = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        System.out.println(wordCount(line));
+        System.out.println(specificwordcount(line,"Hamlet"));
+    }
+
+
 }
